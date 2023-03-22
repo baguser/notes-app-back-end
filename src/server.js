38baf10b34592ebfd7c5f0ecd6dbdaf32c4f4e81
baguser <baguseririzqiyan@gmail.com>
2023-03-22 11:56:39 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ const routes = require("./routes");
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
+    // jika mengikuti menggunakan amazon ubah kode berikut di bawah ini menjadi 0,0,0,0
+    // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     host: "localhost",
     // supaya tidak error/tidak bisadijalankan aplikasinya karena berbeda port kita tambahkan routes dibawah ini
     routes: {
